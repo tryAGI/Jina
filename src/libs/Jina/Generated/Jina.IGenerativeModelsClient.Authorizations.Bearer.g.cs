7ff5c6b6d911a3d79367b1dba49f18fe19968137
Konstantin S. <havendv@gmail.com>
2026-09-13 07:01:47 +0400

    feat: Updated OpenAPI spec (#834)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Jina/Generated/Jina.IGenerativeModelsClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace Jina
+{
+    public partial interface IGenerativeModelsClient
+    {
+        /// <summary>
+        /// Authorize using bearer authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingBearer(
+            string apiKey);
+    }
+}
